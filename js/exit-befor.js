26c1e307b3e87a0acc855c9a82441df1f718fa47
russian-achilles <o232079t@mail.kyutech.jp>
2018-06-17 05:23:21 +0900

    exit.jpでの比較方法を変更

--- js/exit-befor.js
+++ js/exit-befor.js
@@ -3,20 +3,9 @@ $(function(){
   friend_id = window.sessionStorage.getItem('friend_id');
   milkcocoa = new MilkCocoa("yieldjiag7jt5.mlkcca.com");
   name_ds = milkcocoa.dataStore("namelist");
-  if(user_id == void 0){
-    location.href="login.html";
-  }
-  else if (friend_id == void 0) {
-    location.href="login.html";
-  }
   name_ds.get(user_id,function(err, datum){
     if(err){
       location.href="login.html";
-    }
-  })
-  name_ds.get(friend_id,function(err,datum){
-    if(err){
-      location.href="serch.html";
     }
   })
 })
